e8ad4579fac4c45043d64bcb83f86b89ca811676
sus <sus@gmail.com>
2022-09-06 19:37:28 +0530

    form

---
+++ src/App.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-app>
+    <v-form>
+     
+
+      <v-text-field 
+          placeholder="Name"
+          v-model="name"
+          label="Name"
+          :rules="namerule"
+          filled
+          required
+          >
+        </v-text-field>
+ 
+      
+       <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        filled
+        required
+        >
+        </v-text-field>
+      
+        <label>Gender</label>
+  
+        <v-radio-group v-model="radioGroup">
+        <v-radio
+          v-for="(choice, i) in choice"
+          :key="i"
+          :label="choice"
+          :value="choice"
+          :choice="choice"
+          required
+          
+        ></v-radio>
+        </v-radio-group>
+        
+        
+        <v-combobox
+        v-model="course"
+          :items="items"
+          label="Select Course"
+          :rules="select"
+          required
+          
+        ></v-combobox>
+    <label>Hobbies</label>
+    <v-checkbox-group>
+        <v-checkbox
+          v-for="(hobbies, i) in hobbies"
+          :key="i"
+          :label="hobbies"
+          :value="hobbies"
+          :item="hobbies"
+          v-model="checkboxGroup"
+          required
+          
+        ></v-checkbox>
+      </v-checkbox-group>
+     <!-- <label>Hobbies</label>
+      <v-checkbox
+      v-mo del="hobbies"
+      
+      label="Reading"
+      value="Reading"
+    ></v-checkbox>
+    <v-checkbox
+    
+      label="playing"
+      value="playing"
+    ></v-checkbox>
+    <v-checkbox
+      label="cooking"
+      value="social"
+    ></v-checkbox> -->
+
+    <v-btn
+      depressed
+      color="primary"
+      @click="submit"
+    >
+      Submit
+    </v-btn>
+    
+  </v-form>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  data: () => ({
+      name:"",
+      namerule:[
+        name=>!!name||'Name is required'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      choice:['Female','Male'],
+      radioGroup:'',
+      items: ['IT','CSE','EEE','ECE'],
+      course:'',
+      select:[
+      v => !!v || 'course is required',
+      ],
+      hobbies:['reading','playing','swimming','cooking'],
+      checkboxGroup:[],
+      result: [],
+    }),
+    methods:{
+      submit(){
+        this.result.push({
+          name: this.name,
+          email: this.email,
+          gender: this.radioGroup,
+          course:this.course,
+          hobbies:this.checkboxGroup
+        })
+        console.log(JSON.stringify(this.result));
+      }
+    }
+};
+</script>
