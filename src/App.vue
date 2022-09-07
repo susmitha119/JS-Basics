18e6c7c6d3966b6d73fdf42c736c946ac5f53fa8
susmitha119 <110808869+susmitha119@users.noreply.github.com>
2022-09-07 11:59:42 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,82 +9,58 @@
           label="Name"
           :rules="namerule"
           filled
-     ></v-text-field>
- 
+      ></v-text-field>
       
-    <v-text-field
+      
+      <v-text-field
         v-model="email"
         :rules="emailRules"
         label="E-mail"
         filled
-    ></v-text-field>     
+       ></v-text-field>  
         
        
-   <label>Gender</label>
-   <v-radio-group v-model="radioGroup">
+      <label>Gender</label>
+      <v-radio-group v-model="radioGroup">
         <v-radio
           v-for="(choice, i) in choice"
           :key="i"
           :label="choice"
           :value="choice"
           :choice="choice"
-          required
-        ></v-radio>
-   </v-radio-group>
-        
-   <v-combobox
+      ></v-radio>
+      </v-radio-group>
+       
+       
+      <v-combobox
         v-model="course"
           :items="items"
           label="Select Course"
           :rules="select"
-  ></v-combobox>
-  
-  
-    <label>Hobbies</label>
-    <v-checkbox-group>
+       
+      ></v-combobox>
+      
+     <label>Hobbies</label>
         <v-checkbox
+        v-model="checkbox"
           v-for="(hobbies, i) in hobbies"
           :key="i"
           :label="hobbies"
           :value="hobbies"
           :item="hobbies"
-          v-model="checkboxGroup"
-        
-          
-        ></v-checkbox>
-      </v-checkbox-group>
-     <!-- <label>Hobbies</label>
-      <v-checkbox
-      v-mo del="hobbies"
-      
-      label="Reading"
-      value="Reading"
-    ></v-checkbox>
-    <v-checkbox
-    
-      label="playing"
-      value="playing"
-    ></v-checkbox>
-    <v-checkbox
-      label="cooking"
-      value="social"
-    ></v-checkbox> -->
-
-    <v-btn
-      depressed
-      color="primary"
-      @click="submit"
-    >
-      Submit
-    </v-btn>
-    
-  </v-form>
+        ></v-checkbox>  
+         <v-btn  
+            depressed
+            color="primary"
+            @click="submit"
+         >
+         Submit
+       </v-btn>  
+      </v-form>
   </v-app>
 </template>
-
-<script>
-
-
+ 
+ <script>
 export default {
   name: 'App',
 
@@ -93,22 +69,28 @@ export default {
       namerule:[
         name=>!!name||'Name is required'
       ],
+      
       email: '',
       emailRules: [
         v => !!v || 'E-mail is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
+      
       choice:['Female','Male'],
       radioGroup:'',
+      
       items: ['IT','CSE','EEE','ECE'],
       course:'',
-      select:[
-      v => !!v || 'course is required',
+      select:[ 
+          v => !!v || 'course is required',
       ],
+      
       hobbies:['reading','playing','swimming','cooking'],
-      checkboxGroup:[],
+      checkbox:[],
       result: [],
     }),
+    
+    
     methods:{
       submit(){
         this.result.push({
@@ -116,7 +98,7 @@ export default {
           email: this.email,
           gender: this.radioGroup,
           course:this.course,
-          hobbies:this.checkboxGroup
+          hobbies:this.checkbox
         })
         console.log(JSON.stringify(this.result));
       }
